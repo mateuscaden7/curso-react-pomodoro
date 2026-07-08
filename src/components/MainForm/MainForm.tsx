feac60629e89bac24170eba38f69cf5456d3f04c
mateuscaden7 <mateuscaden@hotmail.com>
2026-07-08 16:30:30 -0300

    feature: Adicionando valores de task no contexto ao dar submit no form

--- src/components/MainForm/MainForm.tsx
+++ src/components/MainForm/MainForm.tsx
@@ -2,12 +2,67 @@ import { PlayCircleIcon } from "lucide-react"
 import { Cycles } from "../Cycles"
 import { DefaultButton } from "../DefaultButton"
 import { LabelInput } from "../LabelInput"
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 
 export function MainForm() {
+    const taskNameInput = useRef<HTMLInputElement>(null);
+    const { state, setState } = useTaskContext();
+
+    const nextCycle = getNextCycle(state.currentCycle);
+    const nextCycleType = getNextCycleType(nextCycle);
+
+    function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        if (taskNameInput.current === null) return; 
+
+        const taskName = taskNameInput.current.value.trim();
+
+        if (taskName === "") {
+            alert('Digite o nome da tarefa');
+            return;
+        }
+        
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: taskName,
+            duration: state.config[nextCycleType],
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            type: nextCycleType
+        }
+
+        const secondsRemaining = newTask.duration * 60;
+
+        setState(prevState => {
+            return {
+                ...prevState,
+                activeTask: newTask,
+                currentCycle: nextCycle,
+                secondsRemaining,
+                formattedSecondsRemaining: `${(Math.trunc(secondsRemaining / 60).toString().padStart(2, '0'))}:${((secondsRemaining % 60).toString().padStart(2, '0'))}`,
+                tasks: [
+                    ...prevState.tasks,
+                    newTask
+                ]
+            }
+        });
+    }
+
     return (
-        <form className="form" action="">
+        <form onSubmit={handleCreateNewTask} className="form" action="">
             <div className="formRow">
-                <LabelInput topLabel="Task" placeholder="Digite algo..." id="inputTask"/>
+                <LabelInput 
+                    topLabel="Task" 
+                    placeholder="Digite algo..." 
+                    id="inputTask"
+                    ref={taskNameInput}
+                />
             </div>
 
             <div className="formRow">
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { Container } from "../../components/Container";
 import { Countdown } from "../../components/Countdown";
 import { MainForm } from "../../components/MainForm";
-import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 
 export function Home() {
